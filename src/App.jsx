47702fe238d9ec43ab60f4bc21c6571e2fd945a8
Pedro Torres <118758354+pedrotorres1001@users.noreply.github.com>
2025-08-06 22:22:04 +0100

    por enquanto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import Navbar from './sections/Navbar'
 
 const App = () => {
-  return <div>App</div>
+  return <div className='container mx-auto max-w-7xl'>
+    <Navbar />
+    {/*hero*/}
+    {/*about*/}
+    {/*projects*/}
+    {/*experience*/}
+    {/*testimonials*/}
+    {/*contact*/}
+    {/*footer*/}
+  </div>
 }
 
 export default App
